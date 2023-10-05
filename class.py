2bccdd26d5d9a2b29747f312d3a5d9d55056b29d
Abhikrishna S <117815038+s-abhikrishna@users.noreply.github.com>
2023-10-05 14:44:09 +0530

    Delete class.py

--- class.py
+++
@@ -1,8 +0,0 @@
-class Greeter:
-    def _init__(self, name):
-        self.name=name
-    def greet(self,loud=False):
-        if loud:
-            print("HELLO",self.name.upper())
-        else:
-            print("HELLO",self.name.lower())
